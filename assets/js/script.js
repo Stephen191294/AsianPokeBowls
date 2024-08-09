82e7b6e8ba3df1ff7e2027daa210ff9150edab68
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-08-09 14:49:29 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,78 @@
 document.addEventListener('DOMContentLoaded', () => {
   const element = document.getElementById('fadeElement');
-  fadeIn(element, 2000); // Duration in milliseconds
+  fadeIn(element, 2000); 
 });
 
 function fadeIn(element, duration) {
   let opacity = 0;
-  const interval = 50; // Interval time in milliseconds
+  const interval = 50;
   const increment = interval / duration;
 
   const fade = setInterval(() => {
       opacity += increment;
       if (opacity >= 1) {
           clearInterval(fade);
-          opacity = 1; // Ensure the final opacity is exactly 1
+          opacity = 1; 
       }
       element.style.opacity = opacity;
   }, interval);
-}
+};
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hoverBox = document.querySelector('.box-1');
+  const icon = document.getElementById('icon');
+
+  hoverBox.addEventListener('mouseover', () => {
+    icon.style.color = '#ffffff'; 
+  });
+
+  hoverBox.addEventListener('mouseout', () => {
+    icon.style.color = '#000000'; 
+  });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hoverBox = document.querySelector('.box-2');
+  const icon = document.getElementById('icon-2');
+
+  hoverBox.addEventListener('mouseover', () => {
+    icon.style.color = '#ffffff'; 
+  });
+
+  hoverBox.addEventListener('mouseout', () => {
+    icon.style.color = '#000000'; 
+  });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hoverBox = document.querySelector('.box-3');
+  const icon = document.getElementById('icon-3');
+
+  hoverBox.addEventListener('mouseover', () => {
+    icon.style.color = '#ffffff'; 
+  });
+
+  hoverBox.addEventListener('mouseout', () => {
+    icon.style.color = '#000000'; 
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hoverBox = document.querySelector('.box-4');
+  const icon = document.getElementById('icon-4');
+
+  hoverBox.addEventListener('mouseover', () => {
+    icon.style.color = '#ffffff'; 
+  });
+
+  hoverBox.addEventListener('mouseout', () => {
+    icon.style.color = '#000000'; 
+  });
+});
