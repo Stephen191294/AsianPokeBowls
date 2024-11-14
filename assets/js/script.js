13686e606040edffec820d94bfb1019f908b3b0d
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-11-14 16:05:41 -0300

    build box update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,27 @@
-new WOW().init();
+new WOW().init()
 
-
-function showSidebar(){
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "flex"
-
+function showSidebar () {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
 }
 
-
-function hideSidebar(){
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "none"
-
+function hideSidebar () {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
 }
 
+const menuBody = document.getElementById('menuPickUpBody')
+const buildBowlBtn = document.getElementById('buildBowl')
+const buildBowlBox = document.getElementById('BuildBowlBox')
+const closeBtn = document.getElementById('closeBtn')
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const hoverBox = document.querySelector('.box-1');
-  const icon = document.getElementById('icon');
-
-  hoverBox.addEventListener('mouseover', () => {
-    icon.style.color = '#ffffff'; 
-  });
-
-  hoverBox.addEventListener('mouseout', () => {
-    icon.style.color = '#000000'; 
-  });
+buildBowlBtn.addEventListener('click', () => {
+  
+  buildBowlBox.style.display = (buildBowlBox.style.display === 'none' || buildBowlBox.style.display === '') ? 'block' : 'none';
 });
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const hoverBox = document.querySelector('.box-2');
-  const icon = document.getElementById('icon-2');
-
-  hoverBox.addEventListener('mouseover', () => {
-    icon.style.color = '#ffffff'; 
-  });
-
-  hoverBox.addEventListener('mouseout', () => {
-    icon.style.color = '#000000'; 
-  });
+closeBtn.addEventListener('click', () => {
+  
+  buildBowlBox.style.display = 'none';
 });
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  const hoverBox = document.querySelector('.box-3');
-  const icon = document.getElementById('icon-3');
-
-  hoverBox.addEventListener('mouseover', () => {
-    icon.style.color = '#ffffff'; 
-  });
-
-  hoverBox.addEventListener('mouseout', () => {
-    icon.style.color = '#000000'; 
-  });
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const hoverBox = document.querySelector('.box-4');
-  const icon = document.getElementById('icon-4');
-
-  hoverBox.addEventListener('mouseover', () => {
-    icon.style.color = '#ffffff'; 
-  });
-
-  hoverBox.addEventListener('mouseout', () => {
-    icon.style.color = '#000000'; 
-  });
-});
