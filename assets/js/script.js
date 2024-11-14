f7a22a1d229a5ea380f75e9747e281910ee14c68
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-11-14 18:53:39 -0300

    login box complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,14 @@ closeBtn.addEventListener('click', () => {
   buildBowlBox.style.display = 'none';
 });
 
+const loginBox = document.getElementById('loginBoxOne');
+const signInButton = document.getElementById('signInBtn');
+const loginCloseButton = document.getElementById('loginCloseBtn');
+
+signInButton.addEventListener('click', () => {
+  loginBox.style.display = 'flex';
+});
+
+loginCloseButton.addEventListener('click', () => {
+  loginBox.style.display = 'none';
+});
